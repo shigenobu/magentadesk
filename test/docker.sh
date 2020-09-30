cc7efcc44569e3feac94bbb88c8b60324ef97e47
Shigenobu Furuta <shigenobu@walksocket.com>
2020-09-30 15:34:24 +0900

    Version0.1.5.
    * Add tested on MariaDB 10.5.

--- test/docker.sh
+++ test/docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run --name magentadesk-`date +%s` -v `pwd`/conf.d:/etc/mysql/conf.d -p 13306:3306 -e MYSQL_ROOT_PASSWORD=pass -d mariadb:10.4
+docker run --name magentadesk-`date +%s` -v `pwd`/conf.d:/etc/mysql/conf.d -p 13306:3306 -e MYSQL_ROOT_PASSWORD=pass -d mariadb:10.5
 sleep 10
 
 mysql -h 127.0.0.1 -P 13306 -u root -ppass -e "drop database if exists base; create database base default charset=utf8mb4;"
