d755dde44e05e19c47b495b937b727e323ff9f1b
Shigenobu Furuta <shigenobu@walksocket.com>
2025-10-26 16:07:53 +0900

    Support for MySQL 8.0.44.

--- cli/src/main/java/com/walksocket/md/MdExecute.java
+++ cli/src/main/java/com/walksocket/md/MdExecute.java
@@ -52,9 +52,10 @@ public class MdExecute {
             || version.contains("8.0.40")
             || version.contains("8.0.41")
             || version.contains("8.0.42")
-            || version.contains("8.0.43"))) {
+            || version.contains("8.0.43")
+            || version.contains("8.0.44"))) {
           throw new MdExceptionInvalidVersion(
-              "MySQL 8.0.31 - 8.0.43 is required.");
+              "MySQL 8.0.31 - 8.0.44 is required.");
         }
       } else {
         // check version
